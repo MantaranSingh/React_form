a56f619bea2495907e422414d702de7f262b5957
MantaranSingh <meekmantaran@gmail.com>
2020-09-16 19:40:30 +0530

    Updates

---
+++ precache-manifest.73b0212aa9f5c4c427fa80063889e1a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28126ed68d1dde2920e9e1956eca4ad",
+    "url": "/React_form/index.html"
+  },
+  {
+    "revision": "cdd4d4648e925a28cbcb",
+    "url": "/React_form/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1bc17455258553fa6d50",
+    "url": "/React_form/static/js/2.38a4cc39.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/React_form/static/js/2.38a4cc39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd4d4648e925a28cbcb",
+    "url": "/React_form/static/js/main.a73ae3e4.chunk.js"
+  },
+  {
+    "revision": "89d807cff81cefd99147",
+    "url": "/React_form/static/js/runtime-main.71580f36.js"
+  }
+]);
